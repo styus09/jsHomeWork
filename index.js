28bd65aea05e1786ee1fb40f024cbddb2b1fc2e5
Sean <styus09@gmail.com>
2021-10-15 10:23:42 -0400

    jsHomeWork

---
+++ index.js
@@ -0,0 +1,25 @@
+var a =10;
+var b =12;
+var c =14;
+var d =27;
+var e =33;
+var f =23;
+var g =9;
+var h =15;
+var i =35;
+var j =12;
+
+console.log(a + b);
+console.log(a + c);
+ 
+console.log(b - g)
+console.log(i - f)
+
+console.log(g * a)
+console.log(e * j)
+
+console.log(d / g)
+console.log(f / a)
+
+console.log(g++)
+console.log(e--)
